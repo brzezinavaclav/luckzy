541e0854621f482536306143b707f6e6a3a26940
brzezinavaclav <brzezina.vaclav@outlook.cz>
2016-12-11 20:16:33 +0100

    new

--- authentication.php
+++ authentication.php
@@ -11,8 +11,8 @@ include __DIR__ . '/inc/ga_class.php';
 <div class="container content">
     <div class="row">
         <div class="col-md-12">
-            <a href="?p=account" style="margin-right: 15px;">Account settings</a>
-            <a href="?p=authentication">Authentication settings</a>
+            <a href="/account" style="margin-right: 15px;">Account settings</a>
+            <a href="/authentication">Authentication settings</a>
             <h1>Two factor authentication setup</h1>
             <?php
             if ($player['ga_token'] == '') {
